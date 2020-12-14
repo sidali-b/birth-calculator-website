93665cd5a05911202dc0b08e3734a2ab1be1f90c
sidalibashir@protonmail.com <sidalibashir@protonmail.com>
2020-12-14 12:40:35 +0300

    add arrays day,female,male

--- java-script/script.js
+++ java-script/script.js
@@ -1,7 +1,17 @@
 function naming(){
 
   let CC = parseInt(document.getElementById("main").value);
-  
+  let YY = parseInt(document.getElementById("name").value);
+  var MM=parseInt(document.getElementById("Form").value);
+  var DD=parseInt(document.getElementById("akan").value);
+
+  var day=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Sartuday"];
+  var femaleNames=["Akosua", "Adwoa","Abenaa","Akua","Yaa"," Afua","Ama"];
+  var maleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+ 
+  if(CC.value =="" || YY.value =="" || MM.value =="" || DD.value ==""){
+      alert("No blank values allowed");
+  }
 
 
 
