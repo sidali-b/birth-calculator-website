c2fc969a1288267c17e532be3a0b813e691d760f
sidalibashir@protonmail.com <sidalibashir@protonmail.com>
2020-12-14 12:42:06 +0300

    let

--- java-script/script.js
+++ java-script/script.js
@@ -2,16 +2,18 @@ function naming(){
 
   let CC = parseInt(document.getElementById("main").value);
   let YY = parseInt(document.getElementById("name").value);
-  var MM=parseInt(document.getElementById("Form").value);
-  var DD=parseInt(document.getElementById("akan").value);
+  let MM=parseInt(document.getElementById("Form").value);
+  let DD=parseInt(document.getElementById("akan").value);
 
-  var day=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Sartuday"];
-  var femaleNames=["Akosua", "Adwoa","Abenaa","Akua","Yaa"," Afua","Ama"];
-  var maleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+  let day=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Sartuday"];
+  let femaleNames=["Akosua", "Adwoa","Abenaa","Akua","Yaa"," Afua","Ama"];
+  let maleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
  
   if(CC.value =="" || YY.value =="" || MM.value =="" || DD.value ==""){
       alert("No blank values allowed");
   }
+
+  let 
 
 
 
